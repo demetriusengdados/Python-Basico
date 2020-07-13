60c332c237e4e99d83160bd72129a5a155e6a0ea
dimagela29 <63015481+dimagela29@users.noreply.github.com>
2020-07-13 13:01:47 -0300

    Add files via upload

--- Desafio 20.py
+++ Desafio 20.py
@@ -1,8 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  9 10:49:58 2020
-
-@author: dmagela
-"""
-
-
+from random import shuffle 
+n1 = str(input('Primeiro aluno; '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+n5 = str(input('Quinto aluno: '))
+n6 = str(input('Sexto aluno: '))
+lista = [n1, n2, n3, n4, n5, n6]
+shuffle(lista)
+print('A ordem de apresentação será: ')
+print(lista)
