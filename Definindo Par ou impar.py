fbc8767e22b2b789fcb6b3b3ead16d067a5e9b20
dimagela29 <63015481+dimagela29@users.noreply.github.com>
2020-07-13 16:07:07 -0300

    Add files via upload

---
+++ Definindo Par ou impar.py
@@ -0,0 +1,7 @@
+numero = int(input('Me diga um numero qualquer: '))
+resultado = numero % 2
+if resultado == 0:
+    print('O numero {} é par'.format(numero))
+else:
+    print('O numero é impar'.format(numero))
+    
