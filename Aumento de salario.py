4871908e4602202f88e9ac024bd68d3b467232ee
dimagela29 <63015481+dimagela29@users.noreply.github.com>
2020-07-13 19:43:20 -0300

    Update Aumento de salario.py

--- Aumento de salario.py
+++ Aumento de salario.py
@@ -3,5 +3,5 @@ if salario <= 1250:
     novo = salario + (salario * 15 / 100)
 else:
     novo = salario + (salario * 10 / 100)
-    print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salario, novo))
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salario, novo))
     
