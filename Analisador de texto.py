363f0af3d66194b2656628d5033e0dacc4f19a4d
dimagela29 <63015481+dimagela29@users.noreply.github.com>
2020-07-13 15:33:04 -0300

    Add files via upload

---
+++ Analisador de texto.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome em maiuscula é {}'.format(nome.upper()))
+print('Seu nome em minuscula é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome)- nome.count(' ')))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
---
+++ Cidade com Santo.py
@@ -0,0 +1,2 @@
+cid = str(input('Em que cidade vc nasceu? ')).strip()
+print(cid[:5].upper()== 'Santo')
