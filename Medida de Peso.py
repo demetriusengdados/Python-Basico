1f79e2ba59eeb88181d2b83acf68eedd3263faa5
dimagela29 <63015481+dimagela29@users.noreply.github.com>
2020-07-20 21:49:40 -0300

    Add files via upload

---
+++ Medida de Peso.py
@@ -0,0 +1,14 @@
+maior = 0
+menor = 0
+for p in range (1, 6):
+    peso = float(input('Peso da {} pessoa: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}kg'.format(maior))
+print('O menor peso lido foi de {}kg'.format(menor))
