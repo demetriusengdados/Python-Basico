988d02c7733ffbd7e7aea3e0f5966359fb1a27ab
Devtrybe <63015481+dimagela29@users.noreply.github.com>
2020-07-28 19:36:25 -0300

    Add files via upload

---
+++ Calculando fatorial.py
@@ -0,0 +1,12 @@
+n = int(input('Digite um numero para cacular seu fatorial: '))
+c = n
+f = 1
+print('Calculando {}!'.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print('x' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
+
+    
