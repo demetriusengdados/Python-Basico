7a21b80221f43a37ac7994d9d2e1417c40510107
Demetrius <63015481+dimagela29@users.noreply.github.com>
2020-09-28 14:10:14 -0300

    Add files via upload

---
+++ Definindo print.py
@@ -0,0 +1,12 @@
+def escreva(msg):
+  tam = len(msg) + 4
+  print('~' * tam)
+  print(f'  {msg}')
+  print('~' * tam)
+  
+  
+#programa principal
+escreva('Demetrius Magela da Mata')
+escreva('Curso em video de Python')
+escreva('Teste e mude o codigo de acordo com sua necessidade')
+escreva('Codigo feito no spyder')
