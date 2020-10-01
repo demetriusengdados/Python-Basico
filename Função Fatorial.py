a352a68ce04e8eeb00cfd0dbdfb3c0e2590e9003
Demetrius <63015481+dimagela29@users.noreply.github.com>
2020-10-01 13:26:40 -0300

    Add files via upload

---
+++ Função Fatorial.py
@@ -0,0 +1,16 @@
+def fatorial(n, show= False):
+  
+  f = 1 
+  for c in range (n, 0, -1):
+    if show:
+      print(c, end='')
+      if c > 1:
+        print(' x ', end='')
+      else:
+        print(' = ', end='')
+    f *= c
+  return f
+
+
+#programa principal
+print(fatorial(5, show=True))
