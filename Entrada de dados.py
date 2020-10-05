a52a66cfc19759398959db0c9cf803fe1560b111
Demetrius <63015481+dimagela29@users.noreply.github.com>
2020-10-05 14:58:11 -0300

    Add files via upload

---
+++ Entrada de dados.py
@@ -0,0 +1,9 @@
+def leiaDinheiro(msg):
+  valido = False
+  while not valido:
+    entrada = str(input(msg)).replace(',', '.')
+    if entrada.isalpha():
+      print(f'\033[0;31mErro: \"{entrada}\" é um preço inválido\033[m')
+    else:
+      valido = True
+      return float(entrada)
