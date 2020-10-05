22453c40d880ed896f9f9e44c69fd2edef3345de
Demetrius <63015481+dimagela29@users.noreply.github.com>
2020-10-05 14:01:12 -0300

    Add files via upload

---
+++ Modulos Python 2.py
@@ -0,0 +1,6 @@
+import Moeda 2
+
+p = float(input('Digite o preço: R$'))
+print(f'A metade de {Moeda.Moeda 2(p)} é {Moeda.meta(p)}')
+print(f'O dobro de R${p} é R${Moeda.dobro(p)}')
+print(f'Aumentando 10% , temos R${Moeda.aumentar(p, 10)}')
---
+++ Moeda 2.py
@@ -0,0 +1,21 @@
+def aumentar(preço = 0, taxa = 0):
+  res = preço + (preço * taxa/100)
+  return res
+
+
+def diminui(preço = 0, taxa = 0):
+  res = preço - (preço * taxa/100)
+  return res
+
+
+def dobro(preço = 0):
+  res = preço * 2
+  return res
+
+
+def metade(preço = 0):
+  res = preço / 2
+  return res 
+
+def moeda(preço = 0, moeda = 'R$'):
+  return f'{moeda}{preço}'.replace('.', ',')
